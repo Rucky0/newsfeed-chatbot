8b7b94cde8a101f652764ca20d031f4105172b85
Rucky0 <zugo789@hotmail.com>
2024-05-02 12:24:18 +0200

    lambda body

--- main.py
+++ main.py
@@ -2,9 +2,38 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+    prompt ="prompt"
+
+    kwargs = {
+                "modelId": "anthropic.claude-3-sonnet-20240229-v1:0",
+                "contentType": "application/json",
+                "accept": "application/json",
+                "body": {
+                    "anthropic_version": "bedrock-2023-05-31",
+                    "max_tokens": 1000,
+                    "messages": [
+                    {
+                        "role": "user",
+                        "content": [
+                        {
+                            "type": "image",
+                            "source": {
+                            "type": "base64",
+                            "media_type": "image/jpeg",
+                            "data": "iVBORw..."
+                            }
+                        },
+                        {
+                            "type": "text",
+                            "text": f"{prompt}?"
+                        }
+                        ]
+                    }
+                    ]
+                }
+                }
     
     return {
         'statusCode': 200,
-        test
         'body': json.dumps('Hello from Lambda!')
     }
